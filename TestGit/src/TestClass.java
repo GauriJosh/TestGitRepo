a1da70bdcf0f0a879270dabef184e3e24bc620f8
ldapadmin <ldapadmin@civcez138.MainDC.com>
2017-02-08 11:44:29 +0530

    removing some code

--- TestGit/src/TestClass.java
+++ TestGit/src/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 		for(int i = 0 ; i<=10; i++){
 			System.out.println("Hellow World : " + i);
 			System.out.println("Wohoooooooooooo");
-			System.out.println("My new changes to code:");
+			
 		}
 	}
 
